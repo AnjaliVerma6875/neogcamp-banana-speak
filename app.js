42275680c28d5a78abd531f2fdd57d839394d3e9
Anjali Verma <anjaliverma6875@gmail.com>
2022-07-04 22:49:28 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
+//outputDiv.innerText="Anjali Verma Good Learning"
 
+//console.log(txtInput);
+console.log(outputDiv);
 //console.log(btnTranslate);
 
 btnTranslate.addEventListener("click", function clickEventhandler(){
-    console.log("Clicked!!! ");
-    console.log("Input", txtInput.value);
+   // console.log("Clicked!!! ");
+    //console.log("Input", txtInput.value);
+    outputDiv.innerText="Anjali Verma Good Learning " + txtInput.value;
+
+
 });
 
+//1. document.querySelectorAll("textarea")
+//2. .btn-primary
+//3. #input-btn
+//4. "input(name='translator')"
