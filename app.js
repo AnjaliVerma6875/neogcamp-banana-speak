68afc5bf3c8b2e825f56c6c6b89ead0ee1922b6f
Anjali Verma <anjaliverma6875@gmail.com>
2022-07-06 18:05:52 +0530

    demopractice

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var outputDiv = document.querySelector("#output");
 
 
 //DEMO var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+//var serverURL = "https://api.funtranslations.com/translate/groot.json"
 
 function getTranslationURL(text){
     return serverURL + "?" + "text=" + text
